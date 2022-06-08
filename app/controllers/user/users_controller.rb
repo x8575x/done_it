88673433e23e80212b74ff486662c3ee0ab8f2c9
x8575x <xx8575xx@gmail.com>
2022-06-08 11:33:05 +0000

    [Add]user/editページ

--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -5,6 +5,21 @@ class User::UsersController < ApplicationController
   end
 
   def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    @user.update(user_params)
+    redirect_to user_path(current_user.id)
+  end
+
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :birthday, :sex, :introduction, :profile_image)
   end
 
 end
