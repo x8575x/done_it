c2565b3a331be49947e14a89ffa02a65d4334bef
x8575x <xx8575xx@gmail.com>
2022-06-12 02:59:03 +0000

    [Add]レビューいいね機能

--- app/controllers/user/favorites_controller.rb
+++ app/controllers/user/favorites_controller.rb
@@ -1,18 +1,18 @@
 class  User::FavoritesController < ApplicationController
 
   def create
-    review = Review.find(params[:review_id])
-    favorite = Favorite.new
+    @review = Review.find(params[:review_id])
+    favorite = Favorite.new(review_id: @review.id)
     favorite.user_id = current_user.id
-    favorite.save
-    redirect_to review_path(review)
+    favorite.save!
+    redirect_to request.referer
   end
 
   def destroy
-    review = Review.find(params[:review_id])
-    favorite = current_user.favorites.find_by(review_id: review.id)
+    @review = Review.find(params[:review_id])
+    favorite = current_user.favorites.find_by(review_id: @review.id)
     favorite.destroy
-    redirect_to review_path(review)
+    redirect_to request.referer
   end
 
 end
