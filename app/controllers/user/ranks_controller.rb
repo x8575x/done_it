91219b831b20d9475d822528e19411c0146a5b6d
x8575x <xx8575xx@gmail.com>
2022-06-17 09:01:38 +0000

    [Add]ランキングページレイアウト

--- app/controllers/user/ranks_controller.rb
+++ app/controllers/user/ranks_controller.rb
@@ -4,18 +4,18 @@ class User::RanksController < ApplicationController
   def rank
     # 読みたい本ランキング
     if (params[:sort]) == 'want'
-      @books = Book.find(Want.group(:book_id).order('count(book_id) desc').pluck(:book_id)).first(10)
+      @books = Book.find(Want.group(:book_id).order('count(book_id) desc').pluck(:book_id)).first(15)
     # 読んでいる本ランキング
     elsif (params[:sort]) == 'reading'
-      @books = Book.find(Reading.group(:book_id).order('count(book_id) desc').pluck(:book_id)).first(10)
+      @books = Book.find(Reading.group(:book_id).order('count(book_id) desc').pluck(:book_id)).first(15)
     # 読んだ本ランキング
     elsif (params[:sort]) == 'read'
-      @books = Book.find(Read.group(:book_id).order('count(book_id) desc').pluck(:book_id)).first(10)
+      @books = Book.find(Read.group(:book_id).order('count(book_id) desc').pluck(:book_id)).first(15)
     # 積読本ランキング
     elsif (params[:sort]) == 'get'
-      @books = Book.find(Get.group(:book_id).order('count(book_id) desc').pluck(:book_id)).first(10)
+      @books = Book.find(Get.group(:book_id).order('count(book_id) desc').pluck(:book_id)).first(15)
     else
-      @books = Book.find(Review.group(:book_id).order('count(book_id) desc').pluck(:book_id)).first(10)
+      @books = Book.find(Review.group(:book_id).order('count(book_id) desc').pluck(:book_id)).first(15)
     end
   end
 
