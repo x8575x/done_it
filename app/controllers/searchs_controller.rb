a8fcb00a746ebc8324bf810ab48d6056dbc3e5fc
x8575x <xx8575xx@gmail.com>
2022-06-17 09:19:00 +0000

    [Add]本の検索方法変更

--- app/controllers/searchs_controller.rb
+++ app/controllers/searchs_controller.rb
@@ -1,13 +1,13 @@
 class SearchsController < ApplicationController
 
   def index
-    if params[:title]
+    if params[:title].present? && params[:author].present?
+      @searchs = RakutenWebService::Books::Book.search(title: params[:title],author: params[:author])
+    elsif params[:title].present?
       @searchs = RakutenWebService::Books::Book.search(title: params[:title])
-    end
-    if params[:author]
+    elsif params[:author].present?
       @searchs = RakutenWebService::Books::Book.search(author: params[:author])
     end
-    @book = Book.new
   end
 
 end
