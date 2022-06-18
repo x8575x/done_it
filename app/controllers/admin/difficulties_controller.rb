42def84aed41724376c2135d3aecb3ee31036686
x8575x <xx8575xx@gmail.com>
2022-06-18 22:34:19 +0000

    [Add]admin/difficultyレイアウト

--- app/controllers/admin/difficulties_controller.rb
+++ app/controllers/admin/difficulties_controller.rb
@@ -3,24 +3,41 @@ class Admin::DifficultiesController < ApplicationController
 
   def create
     @difficulty = Difficulty.new(difficulty_params)
-    @difficulty.save
-    redirect_to admin_top_path
+    if @difficulty.save
+      flash[:notice] = "登録しました"
+      redirect_to admin_difficulties_path
+    else
+      render :index
+    end
   end
 
   def edit
     @difficulty = Difficulty.find(params[:id])
   end
+  
+  def index
+    @difficulty = Difficulty.new
+    @difficulties = Difficulty.all
+  end
 
   def update
     @difficulty = Difficulty.find(params[:id])
-    @difficulty.update(difficulty_params)
-    redirect_to admin_top_path
+    if @difficulty.update(difficulty_params)
+    flash[:notice] = "編集に成功しました"
+      redirect_to admin_difficulties_path
+    else
+      render :index
+    end
   end
 
   def destroy
     @difficulty = Difficulty.find(params[:id])
-    @difficulty.destroy
-    redirect_to admin_top_path
+    if @difficulty.destroy
+      flash[:notice] = "削除"
+      redirect_to admin_difficulties_path
+    else
+      render :index
+    end
   end
 
 
