6582ca3d5924d5e85bf3bcf2c21f2ac4392e2536
x8575x <xx8575xx@gmail.com>
2022-06-18 22:23:04 +0000

    [Add]admin/mysteryレイアウト

--- app/controllers/admin/mysteries_controller.rb
+++ app/controllers/admin/mysteries_controller.rb
@@ -3,24 +3,41 @@ class Admin::MysteriesController < ApplicationController
 
   def create
     @mystery = Mystery.new(mystery_params)
-    @mystery.save
-    redirect_to admin_top_path
+    if @mystery.save
+      flash[:notice] = "登録しました"
+      redirect_to admin_mysteries_path
+    else
+      render :index
+    end
   end
 
   def edit
     @mystery = Mystery.find(params[:id])
   end
+  
+  def index
+    @mystery = Mystery.new
+    @mysteries = Mystery.all
+  end
 
   def update
     @mystery = Mystery.find(params[:id])
-    @mystery.update(mystery_params)
-    redirect_to admin_top_path
+    if @mystery.update(mystery_params)
+      flash[:notice] = "編集に成功しました"
+    redirect_to admin_mysteries_path
+    else
+      render :index
+    end
   end
 
   def destroy
     @mystery = Mystery.find(params[:id])
-    @mystery.destroy
-    redirect_to admin_top_path
+    if @mystery.destroy
+      flash[:notice] = "削除しました"
+      redirect_to admin_mysteries_path
+    else
+      render :index
+    end
   end
 
 
