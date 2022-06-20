64d0ae5392d4f3259ee3f88a06cd69a6e2c995ed
x8575x <xx8575xx@gmail.com>
2022-06-20 13:07:34 +0000

    [Add]データ型変更

--- db/migrate/20220609105822_create_books.rb
+++ db/migrate/20220609105822_create_books.rb
@@ -1,10 +1,10 @@
 class CreateBooks < ActiveRecord::Migration[6.1]
   def change
     create_table :books, id: false do |t|
-      
+
       t.string :title
       t.string :author
-      t.integer :isbn, null: false, primary_key: true
+      t.bigint :isbn, null: false, primary_key: true
       t.string :sales_date
       t.string :publisher_name
       t.text :item_caption
