60b2eb1a96f0ec58dbe05c6e469b8f8b67841a62
x8575x <xx8575xx@gmail.com>
2022-06-21 14:23:22 +0000

    [Add]CSSまとめ

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -4,17 +4,4 @@ class Admin::HomesController < ApplicationController
   def top
   end
 
-  def tags
-    @difficulty = Difficulty.new
-    @difficulties = Difficulty.all
-    @long = Long.new
-    @longs = Long.all
-    @type = Type.new
-    @types = Type.all
-    @mystery = Mystery.new
-    @mysteries = Mystery.all
-    @tag = Tag.new
-    @tags = Tag.all
-  end
-
 end
