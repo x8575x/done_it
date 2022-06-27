0d8657149bd5d7a9055eacb656f0459233ee3fff
x8575x <xx8575xx@gmail.com>
2022-06-27 22:13:29 +0000

    [add]admin/report/show

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -1,3 +1,26 @@
 class Admin::ReportsController < ApplicationController
-  
+  layout 'admin/application'
+
+  def show
+    @report = Report.find(params[:id])
+  end
+
+  def update
+    @report = Report.find(params[:id])
+    if @report.update(report_params)
+      flash[:notice] = "編集に成功しました"
+      redirect_to admin_top_path(@report)
+    else
+      render :show
+    end
+  end
+
+
+  private
+
+
+  def report_params
+    params.require(:report).permit(:is_active)
+  end
+
 end
