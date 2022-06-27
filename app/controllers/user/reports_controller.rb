8d2641a7a46b51f9749f4df7b23374d1e63e4b4c
x8575x <xx8575xx@gmail.com>
2022-06-27 19:01:41 +0000

    [Add]通報機能

--- app/controllers/user/reports_controller.rb
+++ app/controllers/user/reports_controller.rb
@@ -2,13 +2,14 @@ class User::ReportsController < ApplicationController
 
   def new
     @report = Report.new
+    @review_id = params[:review_id]
     # 前ページセッションを保存
     session[:previous_url] = request.referer
   end
 
   def create
     @report = Report.new(report_params)
-    @report_id = current_user.id
+    @report.user_id = current_user.id
     if @report.save
       flash[:notice] = "通報しました"
       # create後にnewの前ページに遷移
