208cc91287b6916bba18429b06297646236772a7
x8575x <xx8575xx@gmail.com>
2022-09-08 12:36:40 +0000

    [add]dm一覧表示 機能完成

--- app/controllers/public/dm_rooms_controller.rb
+++ app/controllers/public/dm_rooms_controller.rb
@@ -2,15 +2,11 @@ class Public::DmRoomsController < ApplicationController
 
   def index
     rooms = DmRelationship.where(user_id: current_user.id)
-    @dm_room = DmRelationship.where(dm_room_id: rooms.ids)
-    # @currentEntries = current_user.dm_relationships
-    # myDm_RoomIds = []
-
-    # @currentEntries.each do |entry|
-    #   myDm_RoomIds << entry.dm_room.id
-    # end
-
-    # @anotherEntries = DmRelationship.where(dm_room_id: myDm_RoomIds).where('user_id != ?',@user.id)
+    my_room_id = []
+    rooms.each do |entry|
+      my_room_id << entry.dm_room.id
+    end
+    @another_entries = DmRelationship.where(dm_room_id: my_room_id).where.not(user_id: current_user.id)
   end
 
 end
