753778080ddea5f3ebeef55f1ffa33e59ca936bb
x8575x <xx8575xx@gmail.com>
2022-09-20 14:50:22 +0000

    [add]dm最新メッセージ表示

--- app/controllers/public/dm_rooms_controller.rb
+++ app/controllers/public/dm_rooms_controller.rb
@@ -7,8 +7,6 @@ class Public::DmRoomsController < ApplicationController
       my_room_id << entry.dm_room.id
     end
     @another_entries = DmRelationship.where(dm_room_id: my_room_id).where.not(user_id: current_user.id).reverse
-    another = DmRelationship.where(dm_room_id: my_room_id)
-    @massege = DirectMessage.where(dm_room_id: another)
   end
 
 end
--- app/helpers/public/dm_rooms_helper.rb
+++ app/helpers/public/dm_rooms_helper.rb
@@ -1,17 +1,3 @@
 module Public::DmRoomsHelper
-  # 最新メッセージのデータを取得して表示するメソッド
-  def most_new_message_preview(entry)
-    # 最新メッセージのデータを取得する
-    message = entry.dm_room.direct_messages.message.order(updated_at: :desc).limit(1)
-    # 配列から取り出す
-    message = message[0]
-    # メッセージの有無を判定
-    if message.present?
-      # メッセージがあれば内容を表示
-      tag.p " #{message} "
-    else
-      # メッセージがなければ[ まだメッセージはありません ]を表示
-      tag.p "[ まだメッセージはありません ]"
-    end
-  end
+  # 
 end
